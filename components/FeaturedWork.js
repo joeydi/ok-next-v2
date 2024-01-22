af6eb684ac29f2c7c499601c8f52449f4ce4d82a
Joe di Stefano <joeydi@okaypl.us>
2024-01-22 01:09:28 -0500

    stuff

--- components/FeaturedWork.js
+++ components/FeaturedWork.js
@@ -5,18 +5,18 @@ export default function FeaturedWork() {
     return (
         <section>
             <div className="container">
-                <div className={styles.heading}>
-                    <h3 className="m-0">Columbia Capital</h3>
-                    <h3 className="m-0">Education Progress Meter</h3>
-                    <h3 className="m-0">Steadkey Mortgage</h3>
-                </div>
+                {/* <div className={styles.heading}>
+                </div> */}
                 <div className="panel min-h-screen section-padding flex flex-col justify-center">
+                    <h3>Columbia Capital</h3>
                     <Image src="/columbia-capital.jpg" width="1755" height="1170" alt="" />
                 </div>
                 <div className="panel min-h-screen section-padding flex flex-col justify-center">
+                    <h3>Education Progress Meter</h3>
                     <Image src="/efa-education-progress-meter.jpg" width="1755" height="1169" alt="" />
                 </div>
                 <div className="panel min-h-screen section-padding flex flex-col justify-center">
+                    <h3>Steadkey Mortgage</h3>
                     <Image src="/steadkey.jpg" width="1360" height="1020" alt="" />
                 </div>
             </div>
