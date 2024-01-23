08e57caf4229b889e259aa8e77609d41ea9e2f19
Joe di Stefano <joeydi@okaypl.us>
2024-01-23 00:30:14 -0500

    add glow to cards

--- components/Card.js
+++ components/Card.js
@@ -3,9 +3,11 @@
 import { useRef } from "react";
 import Link from "next/link";
 import gsap from "gsap";
+import styles from "./Card.module.scss";
 
 export default function Card({ href, target = "_self", children, ...rest }) {
     const cardRef = useRef();
+    const glowRef = useRef();
     const maxRotation = 10;
 
     const mouseMoveHandler = (e) => {
@@ -14,7 +16,9 @@ export default function Card({ href, target = "_self", children, ...rest }) {
         const y = e.clientY - rect.y;
         const rotationX = gsap.utils.mapRange(0, rect.height, -maxRotation, maxRotation, y);
         const rotationY = gsap.utils.mapRange(0, rect.width, maxRotation, -maxRotation, x);
-        console.log(rotationX, rotationY);
+        const glowX = gsap.utils.mapRange(0, rect.width, -100, 0, x);
+        const glowY = gsap.utils.mapRange(0, rect.height, -100, 0, y);
+        console.log(glowX, glowY);
 
         gsap.to(cardRef.current, {
             rotationX,
@@ -23,14 +27,29 @@ export default function Card({ href, target = "_self", children, ...rest }) {
             duration: 0.25,
             ease: "power2.out",
         });
+
+        gsap.to(glowRef.current, {
+            xPercent: glowX,
+            yPercent: glowY,
+            opacity: 0.35,
+            duration: 0.25,
+            ease: "power2.out",
+        });
     };
 
     const mouseLeaveHandler = () => {
-        console.log("mouseLeaveHandler");
         gsap.to(cardRef.current, {
             rotationX: 0,
             rotationY: 0,
             scale: 1,
+            duration: 1,
+            ease: "expo.out",
+        });
+
+        gsap.to(glowRef.current, {
+            xPercent: -50,
+            yPercent: -50,
+            opacity: 0,
             duration: 1,
             ease: "expo.out",
         });
@@ -45,17 +64,18 @@ export default function Card({ href, target = "_self", children, ...rest }) {
     };
 
     return href ? (
-        <div style={{ perspective: "100vw" }}>
+        <div className={`${styles.perspective} relative`}>
             <Link
                 onMouseMove={mouseMoveHandler}
                 onMouseLeave={mouseLeaveHandler}
                 onMouseDown={mouseDownHandler}
                 ref={cardRef}
                 style={{ backgroundColor: "#111", border: "1px solid #222" }}
-                className="block rounded-md overflow-hidden"
+                className="relative block rounded-md overflow-hidden"
                 href={href}
                 target={target}
                 {...rest}>
+                <div ref={glowRef} className={styles.glow}></div>
                 {children}
             </Link>
         </div>
