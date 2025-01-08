4a6cc2fcb15887ec748c3720b283e41254c12e3d
Joe di Stefano <joeydi@okaypl.us>
2025-01-07 22:19:03 -0500

    change body font-weight to medium

--- app/layout.js
+++ app/layout.js
@@ -4,23 +4,23 @@ import Footer from "@/components/Footer";
 import "@/styles/globals.scss";
 
 export const metadata = {
-    title: "Okay Plus » Design & Development For the Web",
-    description: "Elevating non-profits and startups to new heights with 15 years of design and development expertise.",
+  title: "Okay Plus » Design & Development For the Web",
+  description: "Elevating non-profits and startups to new heights with 15 years of design and development expertise.",
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <head>
-                <link rel="stylesheet" href="https://use.typekit.net/gzr7byz.css" />
-            </head>
-            <body>
-                <Header />
-                <SmoothScroll>
-                    {children}
-                    <Footer />
-                </SmoothScroll>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://use.typekit.net/gzr7byz.css" />
+      </head>
+      <body className="font-medium">
+        <Header />
+        <SmoothScroll>
+          {children}
+          <Footer />
+        </SmoothScroll>
+      </body>
+    </html>
+  );
 }
